056de3686c280f5d1a4b04454256c9f463729934
ivignolles <ivanmvig@gmail.com>
2018-06-22 17:09:24 -0300

    todos los labels agregados

---
+++ data/ig02-cars/process_txt.py
@@ -0,0 +1,17 @@
+filename_in = "cars_info.txt"
+filename_out_eval = "cars_eval.txt"
+filename_out_train = "cars_train.txt"
+
+with open(filename_in, 'r') as fp:
+	with open(filename_out_eval, 'w') as fp_oe:
+		with open(filename_out_train, 'w') as fp_ot:
+	
+			lines = fp.readlines()
+			for ln in lines:
+				img = ln.split('.')[0]
+				if ln[0] == ' ':
+					continue
+				if "notraining" in ln and "notest" in ln:
+					fp_oe.write(img+'\n')
+				else:
+					fp_ot.write(img+'\n')
